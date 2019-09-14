ac095897114469cc5d771c9f8c6c255885622740
CptJackFrost <cptjackfrost74@gmail.com>
2019-09-14 16:50:44 +0500

    Icons catalog renamed to hotfix server issues, url in .css midfied. Added missing names to form inputs. Form data sending modified to function using PROMISE. Second form is working now too.

--- js/script.js
+++ js/script.js
@@ -108,5 +108,72 @@ window.addEventListener('DOMContentLoaded', function(){
         document.body.style.overflow = ''; //достаточно для отмены
     });
 
+    //Форма отправки
+
+    let message = {
+        loading: "Загрузка...",
+        success: "Спасибо! Скоро мы с вами свяжемся!",
+        failure: "Кажется, что-то пошло не так..."
+    };
+
+    let form = document.querySelector(".main-form"),
+        formBottom = document.getElementById('form'),
+        input = document.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+    
+    function sendForm(elem){
+        elem.addEventListener('submit', function(event){
+            event.preventDefault();
+            elem.appendChild(statusMessage);
+            
+            let formData = new FormData(elem);
+            let obj = {};
+            formData.forEach(function(value, key){
+                obj[key] = value;
+            });
+
+            let json = JSON.stringify(obj);
+
+            function postData(data){
+                return new Promise(function(resolve, reject){
+                        let request = new XMLHttpRequest();
+                        request.open('POST', 'server.php');
+                        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+                        request.onreadystatechange = function(){
+                            if(request.readyState < 4){
+                            resolve();
+                            } else if(request.readyState === 4){
+                                if(request.status === 200 && request.status < 3){
+                                resolve();
+                            }   else {
+                                    reject();
+                            }
+                        }
+                    };
+                    
+                    request.send(data);
+                });
+            }
+
+            function clearInput(){
+                for(let i = 0; i < input.length; i++){
+                    input[i].value = '';
+                }
+            }
+            
+            postData(json)
+                .then(()=> statusMessage.innerHTML = message.loading)
+                .then(()=> {
+                    statusMessage.innerHTML = message.success;
+                })
+                .catch(()=> statusMessage.innerHTML = message.failure)
+                .then(clearInput);
+        });
+    }
+
+    sendForm(form);
+    sendForm(formBottom);
 
 });
