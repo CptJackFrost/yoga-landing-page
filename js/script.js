0b34783fc9d53957303aee24ee5e19e8a106e160
CptJackFrost <cptjackfrost74@gmail.com>
2019-09-22 16:45:13 +0500

    calculator added

--- js/script.js
+++ js/script.js
@@ -228,4 +228,47 @@ window.addEventListener('DOMContentLoaded', function(){
         }
     });
 
+    //calculator
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        days = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.innerHTML = 0;
+
+    persons.addEventListener('change', function(){
+        personsSum = +this.value;
+        total = (daysSum + personsSum)*1000;
+
+        if(days.value == '' || persons.value == ''){
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    days.addEventListener('change', function(){
+        daysSum = +this.value;
+        total = (daysSum + personsSum)*1000;
+
+        if(persons.value == '' || days.value == ''){
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change', function(){
+        if (days.value == '' || persons.value == ''){
+            totalValue.innerHTML = 0;
+        } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+        }
+    });
+
 });
